17b1122fb9610cd741f435ecd632a24c86e5f0cc
Sergei Pokhodai <s.pokhodai@alef.im>
2022-01-05 20:26:03 +0400

    Initial commit

---
+++ app/src/main/java/com/decorator1889/instruments/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.decorator1889.instruments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
